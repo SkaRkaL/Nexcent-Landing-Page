bfe64d446a6fc0d1510cfeaa9ca96fb2da910b3d
Saad Karkal <karkalsaad@gmail.com>
2025-05-24 19:35:51 +0100

    Add Unlock, Achievements, and Calender components to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,22 @@ import NavBar from "./components/NavBar";
 import HeroSection from "./components/Home/HeroSection";
 import Clients from "./components/Home/Clients";
 import Community from "./components/Home/Community";
+import Unlock from "./components/Body/Unlock";
+import Calender from "./components/Body/Calender";
+import Achievements from "./components/Body/Achievements";
+
 
 export default function Home() {
   return (
-    <div className=" w-full">
+    <div className="w-full">
       <NavBar />
       <main>
         <HeroSection />
         <Clients />
         <Community />
+        <Unlock />
+        <Achievements />
+        <Calender />
       </main>
     </div>
   );
